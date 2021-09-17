8ff606c90efa83ad0b84f603ff519029ac31c6f1
dimapovarchuk <povarchukdima007@gmail.com>
2021-09-17 12:53:35 +0300

    Update!

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo rm -rf /opt/redmine/{,.}*
+sudo tar xvf /opt/redmine.tar.gz -C /opt/redmine/
+sudo cp /opt/configuration.yml /opt/redmine/config
+sudo cp /opt/database.yml /opt/redmine/config
+sudo reboot
