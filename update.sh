7bf739fd60391bdebb7f3f2798abff5b8f11dd2e
Dima Povarchuk <52627259+dimapovarchuk@users.noreply.github.com>
2021-09-17 13:13:32 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sudo rm -rf /opt/redmine/{,.}*
-sudo tar xvf /opt/redmine.tar.gz -C /opt/redmine/
-sudo cp /opt/configuration.yml /opt/redmine/config
-sudo cp /opt/database.yml /opt/redmine/config
-sudo reboot
+
+
+# sudo rm -rf /opt/redmine/{,.}*
+# sudo tar xvf /opt/redmine.tar.gz -C /opt/redmine/
+# sudo cp /opt/configuration.yml /opt/redmine/config
+# sudo cp /opt/database.yml /opt/redmine/config
+# sudo reboot
