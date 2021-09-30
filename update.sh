8cc089f529597df5cdfabd84473b681b8f4b77f9
Dima Povarchuk <52627259+dimapovarchuk@users.noreply.github.com>
2021-09-30 16:06:16 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ sudo tee /opt/redmine/config/database.yml<<EOF
 production:
   adapter: mysql2
   database: redminedb
-  host: 3.10.117.65
+  host: 10.0.1.98
   username: redmineuser
   password: "P@ssW0rD"
   # Use "utf8" instead of "utfmb4" for MySQL prior to 5.7.7
